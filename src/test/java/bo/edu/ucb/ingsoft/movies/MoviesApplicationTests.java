853fb1be47c17080d2a9aa493c53cb17935803fa
Mauricio Illanes <mauricioillanes17@gmail.com>
2022-04-15 19:45:04 -0400

    maven

---
+++ src/test/java/bo/edu/ucb/ingsoft/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.ingsoft.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
