609b9eb66f8a395b5ecfa0b7f15f112c97007af3
Mauricio Illanes <mauricioillanes17@gmail.com>
2022-04-16 16:22:34 -0400

    bot confing

--- src/main/java/bo/edu/ucb/ingsoft/movies/MoviesApplication.java
+++ src/main/java/bo/edu/ucb/ingsoft/movies/MoviesApplication.java
@@ -1,13 +1,40 @@
 package bo.edu.ucb.ingsoft.movies;
 
+import bo.edu.ucb.ingsoft.movies.chat.MoviesLongPollingBot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import javax.sql.DataSource;
+
 
 @SpringBootApplication
+@MapperScan("bo.edu.ucb.ingsoft.movies.dao")
 public class MoviesApplication {
 
+	@Bean
+	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+		SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+		factoryBean.setDataSource(dataSource);
+		return factoryBean.getObject();
+	}
+	
 	public static void main(String[] args) {
-		SpringApplication.run(MoviesApplication.class, args);
+		ApplicationContext context = SpringApplication.run(MoviesApplication.class, args);
+		try {
+			// Inicializamos libreria de bots
+			TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+			// Registramos la implementación de nuestro BOT
+			telegramBotsApi.registerBot(new MoviesLongPollingBot());
+		} catch (TelegramApiException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
---
+++ src/main/java/bo/edu/ucb/ingsoft/movies/chat/MoviesLongPollingBot.java
@@ -0,0 +1,99 @@
+package bo.edu.ucb.ingsoft.movies.chat;
+
+import org.springframework.context.ApplicationContext;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MoviesLongPollingBot extends TelegramLongPollingBot {
+
+    /**
+     * Atributo que sirve para guardar el proceso actual de los diferentes usuarios.
+     */
+    private Map<Long, AbstractProcess> usersSession;
+    private boolean test = false;
+    private List<BotApiMethod> testMessages = new ArrayList<>();
+    private ApplicationContext context;
+
+    public HhRrLongPollingBot(ApplicationContext context) {
+        this.context = context;
+        usersSession = new HashMap<>();
+    }
+
+    public HhRrLongPollingBot(ApplicationContext context, boolean test) {
+        this.context = context;
+        this.test = test;
+        usersSession = new HashMap<>();
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "ucb_movies_2022_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "5315786613:AAEIAcHrRJzYuBjHdUcFhUkv2Nf6Jt_ncGo";
+    }
+
+    public void sendMyMessage(BotApiMethod method) throws TelegramApiException {
+        System.out.println("Enviando mensaje: " + method);
+        if (test) {
+            // no enviamos
+            testMessages.add(method);
+        } else {
+            this.execute(method);
+        }
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        // Primero identifico al usuario por chat Id Long
+        Long chatId = update.getMessage().getChatId();
+        System.out.println("\n\n===========> Recibiendo chatId: " + chatId);
+        // Busco si ya existe Proceso en el map userSession
+        AbstractProcess currentProcess = usersSession.get(chatId);
+
+        if (currentProcess == null) { // Primera vez que se contacto con nostros.
+            System.out.println("Creando proceso para el  chatId: " + chatId);
+            // Debo crear el proceso por defecto
+            currentProcess = new MenuProcessImpl();
+            usersSession.put(chatId, currentProcess);
+            System.out.println("Derivando la conversación al proceso: " + currentProcess.getName());
+            AbstractProcess nextProcess = currentProcess.handle(context, update, this);
+
+            if (!nextProcess.equals(currentProcess)) { // Si el siguiente proceso es diferente lo iniciamos
+                System.out.println("Iniciando siguiente proceso: " + nextProcess.getName());
+                nextProcess.handle(context, update, this);
+            } else {
+                System.out.println("No hay cambio de proceso, así que termina conversación");
+            }
+            usersSession.put(chatId, nextProcess);
+
+        } else { // Ya existe un proceso
+            System.out.println("Continuamos el proceso para el  chatId: " + chatId
+                    + " proceso: " + currentProcess.getName());
+            AbstractProcess nextProcess = currentProcess.handle(context, update, this);
+
+            if (!nextProcess.equals(currentProcess)) { // Si el siguiente proceso es diferente
+                System.out.println("Iniciando siguiente proceso: " + nextProcess.getName());
+                nextProcess = nextProcess.handle(context, update, this);
+            } else {
+                System.out.println("No hay cambio de proceso, así que termina conversación");
+            }
+            usersSession.put(chatId, nextProcess);
+        }
+
+    }
+
+    public List<BotApiMethod> getTestMessages() {
+        return testMessages;
+    }
+}
+
